8be4e9cec55dd7b58175bf3ef3cdd207af0912d2
Carlosmorales27 <82893425+Carlosmorales27@users.noreply.github.com>
2025-01-03 10:35:44 -0600

    Agregamos js

---
+++ Js/Console.js
@@ -0,0 +1,15 @@
+/*se pueden realizar diferentes tipos de inpresiones en consola las principales son
+el console.log funciona para cualquier tipo de impresion en consola
+el console.error es para imprimir mensajes de error en la consola
+el console.warn es para imprimir alertas en consola 
+el console.info es para imprimir mensajes informativos en la consola
+
+*/
+console.log("Hola mundo 🌏");
+
+console.error("Error");
+
+console.warn("Advertencia ⚠");
+
+
+console.info("Informacion ℹ");
